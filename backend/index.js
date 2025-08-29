cbf7e1c7756e46531909ab49bc58b13ab1a4a9e2
avneesh kumar <avneesh1112@gmail.com>
2025-08-29 12:32:38 +0530

    cors fixing

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: ['https://tapnotech.in', 'https://api.technotech.in/create-order']
+  origin: ['https://tapnotech.in', 'https://api.technotech.in/create-order', process.env.FRONTEND_URL, 'https://technotech.in']
 }));
 app.use(express.json());
 
